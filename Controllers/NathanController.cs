4e7e2388e52480ecf17cbfa228c51cc25093e215
Nathan Burns <nate10nate@gmail.com>
2023-11-24 21:28:55 -0500

    implemented add/edit/delete functions

--- Controllers/NathanController.cs
+++ Controllers/NathanController.cs
@@ -14,21 +14,94 @@ public class NathanController : Controller
         data = ctx;
     }
 
-    public ViewResult Index() {
-        try
+    public ViewResult Index()
+    {
+        var gear = data.SnowboardGear
+            .OrderBy(g => g.GearType)
+            .ThenBy(g => g.Brand)
+            .ThenBy(g => g.Model)
+            .ToList();
+        return View(gear);
+
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var gear = data.SnowboardGear.Find(id);
+        return View(gear);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(SnowboardGear gear)
+    {
+        if (ModelState.IsValid)
         {
-            Console.WriteLine("HERE IS THE DATA:", data);
-            var gear = data.SnowboardGear
-                .OrderBy(g => g.GearType)
-                .ThenBy(g => g.Brand)
-                .ThenBy(g => g.Model)
-                .ToList();
+            try
+            {
+                data.Update(gear);
+                data.SaveChanges();
+                TempData["Message"] = $"{gear.Brand} {gear.Model} edited successfully!";
+            }
+            catch
+            {
+                TempData["Message"] = $"Failed to edit {gear.Brand} {gear.Model}.";
+            }
+            return RedirectToAction("Index");
+        }
+        else
+        {
             return View(gear);
         }
-        catch (Exception ex)
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+        var gear = data.SnowboardGear.Find(id);
+        if (gear != null)
         {
-            Console.WriteLine("HERE IS THE ERROR:", ex.Message);
-            throw;
+            try
+            {
+                data.Remove(gear);
+                data.SaveChanges();
+                TempData["Message"] = $"{gear.Brand} {gear.Model} removed successfully!";
+            }
+            catch
+            {
+                TempData["Message"] = $"Failed to remove {gear.Brand} {gear.Model}.";
+            }
         }
+        else
+        {
+            TempData["Message"] = $"Failed to find {id}.";
+        }
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public ViewResult Add()
+    {
+        return View(new SnowboardGear());
+    }
+
+    [HttpPost]
+    public IActionResult Add(SnowboardGear gear)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                data.Add(gear);
+                data.SaveChanges();
+                TempData["Message"] = $"{gear.Brand} {gear.Model} added successfully!";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                TempData["Message"] = $"Failed to add {gear.Brand} {gear.Model}.";
+            }
+        }
+        return View(gear);
     }
 }
