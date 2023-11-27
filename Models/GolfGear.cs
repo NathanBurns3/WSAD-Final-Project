5c3680748a6b9de54c5856420c51148269dfb143
Simon Manzler <manzlesn@mail.uc.edu>
2023-11-27 09:03:10 -0500

    Database added

--- Models/GolfGear.cs
+++ Models/GolfGear.cs
@@ -1,7 +1,11 @@
-﻿namespace WSAD_Final_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSAD_Final_Project.Models
 {
     public class GolfGear
     {
+        [Key]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Brand { get; set; }
         public string Type { get; set; }
