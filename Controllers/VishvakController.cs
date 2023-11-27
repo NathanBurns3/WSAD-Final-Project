a0f0c3964d5b88396bb95adfe37245cffe31db8d
Vishvak C <chintavs@mail.uc.edu>
2023-11-27 15:22:31 -0500

    Added Controller and games connection string

---
+++ Controllers/VishvakController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WSAD_Final_Project.Models;
+
+namespace WSAD_Final_Project.Controllers
+{
+    public class VishvakController : Controller
+    {
+        private GamesContext _context {  get; set; }
+
+        public VishvakController(GamesContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Games);
+        }
+    }
+}
