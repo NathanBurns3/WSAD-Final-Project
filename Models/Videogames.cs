303f0e2f938130b9a87c23817d7609404acd232f
Vishvak C <chintavs@mail.uc.edu>
2023-11-27 15:04:16 -0500

    Videogame Model

---
+++ Models/Videogames.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSAD_Final_Project.Models
+{
+    public class Videogames
+    {
+        [Key]
+        [Required(ErrorMessage = "Please Enter a Game ID")]
+        public int GameId { get; }
+
+        [Required(ErrorMessage = "Please Enter a Game Title")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Game Developer")]
+        public string? Developer { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Game Release Year")]
+        public string? ReleaseYear { get; set; }
+    }
+}
