76b7df8e19a9c7b41d0184b0e3ca6437062dc5c9
Simon Manzler <manzlesn@mail.uc.edu>
2023-11-29 22:03:53 -0500

    Added Add, Edit and Delete

--- Controllers/SimonController.cs
+++ Controllers/SimonController.cs
@@ -12,7 +12,86 @@ namespace WSAD_Final_Project.Controllers
         }
         public IActionResult Index()
         {
-            return View(_context.golfGears.ToList());
+            var gear = _context.golfGears
+            .OrderBy(g => g.Id)
+            .ThenBy(g => g.Name)
+            .ToList();
+            return View(gear);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var gear = _context.golfGears.Find(id);
+            return View(gear);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(GolfGear gear)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gear);
+                    _context.SaveChanges();
+                    TempData["Message"] = $"{gear.Name} edited successfully!";
+                }
+                catch
+                {
+                    TempData["Message"] = $"Failed to edit {gear.Name}.";
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(gear);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var gear = _context.golfGears.Find(id);
+            if (gear != null)
+            {
+                try
+                {
+                    _context.Remove(gear);
+                    _context.SaveChanges();
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Falied to remove gear");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Falied to find id");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ViewResult Add() => View(new GolfGear());
+
+        [HttpPost]
+        public IActionResult Add(GolfGear gear)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(gear);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Falied to add gear");
+                }
+            }
+            return View(gear);
         }
     }
 }
